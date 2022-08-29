cf19d3df29d21d6ff0defd877cdd3f52fc7457f4
michel <michel.chamarez@gmail.com>
2022-08-29 22:01:16 +0200

    Agregar archivos de proyecto.

---
+++ UniversityApiBackend/DataAccess/UniversityDBContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using UniversityApiBackend.Models;
+
+namespace UniversityApiBackend.DataAccess
+{
+    public class UniversityDBContext :DbContext
+    {
+        public UniversityDBContext(DbContextOptions<UniversityDBContext> options) : base(options)
+        { 
+        }
+
+        public DbSet<User>? Users { get; set; }
+
+    }
+}
---
+++ UniversityApiBackend/Models/User.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Build.Framework;
+using System.ComponentModel.DataAnnotations;
+using UniversityApiBackend.Models.DataModels;
+using RequiredAttribute = System.ComponentModel.DataAnnotations.RequiredAttribute;
+
+namespace UniversityApiBackend.Models
+{
+    public class User : BaseEntity
+    {
+        [Required, StringLength(50)]
+        public string Name { get; set; } = String.Empty;
+
+        [Required, StringLength(50)]
+        public string LastName { get; set; } = String.Empty;
+
+        [Required, EmailAddress]
+        public string Email { get; set; } = String.Empty;
+        [Required]
+        public string Password { get; set; } = String.Empty;
+
+    }
+}
